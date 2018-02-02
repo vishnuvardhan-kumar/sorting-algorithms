1d288f8765ef001333fd37a5bc5ad789c3b4330c
Vishnuvardhan Kumar <vishnukumar1997@gmail.com>
2018-02-02 17:52:57 +0000

    Added merge sort implementation

---
+++ merge_sort.py
@@ -0,0 +1,23 @@
+# Python implementation of Merge Sort
+# www.github.com/vishnuvardhan-kumar/sorting-algorithms
+
+def merge_sort(left, right):
+    result = []
+    left_idx, right_idx = 0, 0
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] <= right[right_idx]:
+            result.append(left[left_idx])
+            left_idx += 1
+        else:
+            result.append(right[right_idx])
+            right_idx += 1
+ 
+    if left_idx < len(left):
+        result.extend(left[left_idx:])
+    if right_idx < len(right):
+        result.extend(right[right_idx:])
+    return result
+
+
+
+ 
