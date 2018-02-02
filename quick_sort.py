4295ebeaef864f93fd62abce4504c3e3a757579c
Vishnuvardhan Kumar <vishnukumar1997@gmail.com>
2018-02-02 17:53:18 +0000

    Added quick sort implementation

---
+++ quick_sort.py
@@ -0,0 +1,22 @@
+# Python implementation of Quick Sort
+# www.github.com/vishnuvardhan-kumar/sorting-algorithms
+
+def quick_sort(arr):
+    less = []
+    pivotList = []
+    more = []
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        for i in arr:
+            if i < pivot:
+                less.append(i)
+            elif i > pivot:
+                more.append(i)
+            else:
+                pivotList.append(i)
+        less = quick_sort(less)
+        more = quick_sort(more)
+        return less + pivotList + more
+ 
