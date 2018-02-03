891204a9379e83db32c029e6b105510c6a583f83
Vishnuvardhan Kumar <vishnukumar1997@gmail.com>
2018-02-03 15:11:02 +0000

    Completed benchmarking

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,6 +1,13 @@
 # Python implementation of Insertion Sort
 # www.github.com/vishnuvardhan-kumar/sorting-algorithms
 
+# Average performance on the data set
+# Near : 2.20 seconds
+# Random : 2.47 seconds
+# Reverse : 3.25 seconds
+
+from timeit import default_timer as timer
+
 def insertion_sort(seq):
     for i in range(1, len(seq)):
         key = seq[i]
@@ -14,5 +21,21 @@ def insertion_sort(seq):
         seq[:] = seq[:low] + [key] + seq[low:i] + seq[i + 1:]
 
 
+if __name__ == '__main__':
 
+    time_taken = 0
+    x = int(input("Enter number of trials : "))
+    
+    for n in range(1,x+1):
+        with open("data/random.txt","r") as fileobj:
+            list_to_sort = list(map(int, fileobj.readlines()))
+            start = timer()
+            insertion_sort(list_to_sort)
+            current = timer()-start
+            print(f"Running test {n} : {current:.2f} seconds")
+            time_taken += current
+    
+
+    time_taken /= x
+    print(f"Insertion sort: average time = {time_taken:.2f} seconds")
  
