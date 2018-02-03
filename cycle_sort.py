d62cfdd761f812e896be9ddab7a0da778284da68
Vishnuvardhan Kumar <vishnukumar1997@gmail.com>
2018-02-03 15:10:25 +0000

    Completed benchmarking

--- cycle_sort.py
+++ cycle_sort.py
@@ -1,6 +1,13 @@
 # Python implementation of Cycle Sort adapted from RosettaCode
 # www.github.com/vishnuvardhan-kumar/sorting-algorithms
 
+# Average performance on the data set
+# Near : 8.45 seconds
+# Random : 10.36 seconds
+# Reverse : 9.40 seconds
+
+from timeit import default_timer as timer
+
 def cycle_sort(vector):
     writes = 0
     for cycleStart, item in enumerate(vector):
@@ -26,6 +33,24 @@ def cycle_sort(vector):
  
     return writes
 
+
+if __name__ == '__main__':
+
+    time_taken = 0
+    x = int(input("Enter number of trials : "))
+    
+    for n in range(1,x+1):
+        with open("data/reverse.txt","r") as fileobj:
+            list_to_sort = list(map(int, fileobj.readlines()))
+            start = timer()
+            w = cycle_sort(list_to_sort)
+            current = timer()-start
+            print(f"Running test {n} : {current:.2f} seconds")
+            time_taken += current
+    
+
+    time_taken /= x
+    print(f"Cycle sort: average time = {time_taken:.2f} seconds")       
 
 
 
