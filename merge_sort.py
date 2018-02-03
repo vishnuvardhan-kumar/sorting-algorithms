f14060b6b89604a0bf44d50554c37d5740961cf7
Vishnuvardhan Kumar <vishnukumar1997@gmail.com>
2018-02-03 15:11:20 +0000

    Completed benchmarking

--- merge_sort.py
+++ merge_sort.py
@@ -1,23 +1,43 @@
 # Python implementation of Merge Sort
 # www.github.com/vishnuvardhan-kumar/sorting-algorithms
 
-def merge_sort(left, right):
-    result = []
-    left_idx, right_idx = 0, 0
-    while left_idx < len(left) and right_idx < len(right):
-        if left[left_idx] <= right[right_idx]:
-            result.append(left[left_idx])
-            left_idx += 1
-        else:
-            result.append(right[right_idx])
-            right_idx += 1
+# Average performance on the data set
+# Near : 0.05 seconds
+# Random : 0.09 seconds
+# Reverse : 0.07 seconds
+
+from timeit import default_timer as timer
+from heapq import merge
  
-    if left_idx < len(left):
-        result.extend(left[left_idx:])
-    if right_idx < len(right):
-        result.extend(right[right_idx:])
-    return result
+def merge_sort(m):
+    if len(m) <= 1:
+        return m
+ 
+    middle = len(m) // 2
+    left = m[:middle]
+    right = m[middle:]
+ 
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return list(merge(left, right))
 
 
+if __name__ == '__main__':
+
+    time_taken = 0
+    x = int(input("Enter number of trials : "))
+    
+    for n in range(1,x+1):
+        with open("data/reverse.txt","r") as fileobj:
+            list_to_sort = list(map(int, fileobj.readlines()))
+            start = timer()
+            r = merge_sort(list_to_sort, )
+            current = timer()-start
+            print(f"Running test {n} : {current:.2f} seconds")
+            time_taken += current
+    
+
+    time_taken /= x
+    print(f"Merge sort: average time = {time_taken:.2f} seconds")
 
  
