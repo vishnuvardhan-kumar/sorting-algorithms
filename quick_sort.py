f2da1656b37fe4174f186c8857ba1ec23ba14f39
Vishnuvardhan Kumar <vishnukumar1997@gmail.com>
2018-02-03 15:11:37 +0000

    Completed benchmarking

--- quick_sort.py
+++ quick_sort.py
@@ -1,22 +1,46 @@
 # Python implementation of Quick Sort
 # www.github.com/vishnuvardhan-kumar/sorting-algorithms
 
-def quick_sort(arr):
-    less = []
-    pivotList = []
-    more = []
-    if len(arr) <= 1:
-        return arr
-    else:
-        pivot = arr[0]
-        for i in arr:
-            if i < pivot:
-                less.append(i)
-            elif i > pivot:
-                more.append(i)
-            else:
-                pivotList.append(i)
-        less = quick_sort(less)
-        more = quick_sort(more)
-        return less + pivotList + more
+# Average performance on the data set
+# Near : 0.06 seconds
+# Random : 0.03 seconds
+# Reverse : 0.04 seconds
+
+from timeit import default_timer as timer
+
+def quick_sort(array):
+    _quicksort(array, 0, len(array) - 1)
  
+def _quicksort(array, start, stop):
+    if stop - start > 0:
+        pivot, left, right = array[start], start, stop
+        while left <= right:
+            while array[left] < pivot:
+                left += 1
+            while array[right] > pivot:
+                right -= 1
+            if left <= right:
+                array[left], array[right] = array[right], array[left]
+                left += 1
+                right -= 1
+        _quicksort(array, start, right)
+        _quicksort(array, left, stop)
+
+
+if __name__ == '__main__':
+
+    time_taken = 0
+    x = int(input("Enter number of trials : "))
+    
+    for n in range(1,x+1):
+        with open("data/reverse.txt","r") as fileobj:
+            list_to_sort = list(map(int, fileobj.readlines()))
+            start = timer()
+            quick_sort(list_to_sort)
+            current = timer()-start
+            print(f"Running test {n} : {current:.2f} seconds")
+            time_taken += current
+    
+
+    time_taken /= x
+    print(f"Quick sort: average time = {time_taken:.2f} seconds")
